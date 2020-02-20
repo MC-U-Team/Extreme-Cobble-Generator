8a9b1aba28fa1ed72c4533707aad2d7d57cda0ec
HyCraftHD <hycrafthd@live.de>
2020-02-20 15:36:45 +0100

    Register screen

--- src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorScreens.java
+++ src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorScreens.java
@@ -1,6 +1,8 @@
 package info.u_team.extreme_cobble_generator.init;
 
 import info.u_team.extreme_cobble_generator.ExtremeCobbleGeneratorMod;
+import info.u_team.extreme_cobble_generator.screen.CobbleGeneratorScreen;
+import net.minecraft.client.gui.ScreenManager;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -12,7 +14,7 @@ public class ExtremeCobbleGeneratorScreens {
 	
 	@SubscribeEvent
 	public static void register(FMLClientSetupEvent event) {
-		
+		ScreenManager.registerFactory(ExtremeCobbleGeneratorContainerTypes.GENERATOR, CobbleGeneratorScreen::new);
 	}
 	
 }
