900c8ecab5b3171def41113c63fd1a1698ca4df7
HyCraftHD <hycrafthd@live.de>
2020-02-20 13:41:58 +0100

    Remove old methods for drops that wont work anymore. We will implement the new ones

--- src/main/java/info/u_team/extreme_cobble_generator/block/CobbleGeneratorBlock.java
+++ src/main/java/info/u_team/extreme_cobble_generator/block/CobbleGeneratorBlock.java
@@ -31,7 +31,7 @@ public class CobbleGeneratorBlock extends UTileEntityBlock {
 	// Facing
 	@Override
 	public BlockState getStateForPlacement(BlockItemUseContext context) {
-		return super.getStateForPlacement(context).with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+		return getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
 	}
 	
 	@Override
@@ -67,105 +67,5 @@ public class CobbleGeneratorBlock extends UTileEntityBlock {
 			player.openGui(ExtremeCobbleGeneratorMod.getInstance(), 0, world, pos.getX(), pos.getY(), pos.getZ());
 		}
 		return true;
-	}
-	
-	// Rendering
-	
-	@Override
-	public BlockRenderLayer getRenderLayer() {
-		return BlockRenderLayer.CUTOUT;
-	}
-	
-	public boolean isOpaqueCube(IBlockState state) {
-		return false;
-	}
-	
-	public boolean isFullCube(IBlockState state) {
-		return false;
-	}
-	
-	// NBT
-	
-	@Override
-	public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-		readItemStack(stack, world, pos);
-	}
-	
-	@Override
-	public boolean removedByPlayer(IBlockState state, World world, BlockPos pos, EntityPlayer player, boolean willHarvest) {
-		onPlayerDestroy(world, pos, state);
-		harvestBlock(world, player, pos, state, world.getTileEntity(pos), player.getHeldItemMainhand());
-		world.setBlockToAir(pos);
-		return false;
-	}
-	
-	@Override
-	public void dropBlockAsItemWithChance(World world, BlockPos pos, IBlockState state, float chance, int fortune) {
-		if (!world.isRemote && !world.restoringBlockSnapshots) {
-			NonNullList<ItemStack> drops = NonNullList.create();
-			getDrops(drops, world, pos, world.getBlockState(pos), 0);
-			chance = net.minecraftforge.event.ForgeEventFactory.fireBlockHarvesting(drops, world, pos, state, fortune, chance, false, harvesters.get());
-			
-			for (ItemStack stack : drops) {
-				if (stack.getItem() == Item.getItemFromBlock(this)) {
-					writeItemStack(stack, world, pos);
-				}
-				if (world.rand.nextFloat() <= chance) {
-					spawnAsEntity(world, pos, stack);
-				}
-			}
-		}
-	}
-	
-	@Nonnull
-	@Override
-	public ItemStack getPickBlock(@Nonnull IBlockState state, RayTraceResult target, @Nonnull World world, @Nonnull BlockPos pos, EntityPlayer player) {
-		NonNullList<ItemStack> drops = NonNullList.create();
-		getDrops(drops, world, pos, world.getBlockState(pos), 0);
-		if (drops.size() > 0) {
-			ItemStack stack = drops.get(0);
-			if (stack.getItem() == Item.getItemFromBlock(this)) {
-				writeItemStack(stack, world, pos);
-			}
-			return stack;
-		}
-		return super.getPickBlock(state, target, world, pos, player);
-	}
-	
-	// NBT Util methods
-	
-	private void readItemStack(ItemStack stack, World world, BlockPos pos) {
-		if (!stack.hasTagCompound()) {
-			return;
-		}
-		NBTTagCompound compound = stack.getTagCompound();
-		if (compound == null || !compound.hasKey("TileEntityData", 10)) {
-			return;
-		}
-		NBTTagCompound tiletag = compound.getCompoundTag("TileEntityData");
-		
-		TileEntity tileentity = world.getTileEntity(pos);
-		if (!(tileentity instanceof TileEntityCobbleGenerator)) {
-			return;
-		}
-		TileEntityCobbleGenerator generator = (TileEntityCobbleGenerator) tileentity;
-		generator.readNBT(tiletag);
-	}
-	
-	private void writeItemStack(ItemStack stack, World world, BlockPos pos) {
-		if (!stack.hasTagCompound()) {
-			stack.setTagCompound(new NBTTagCompound());
-		}
-		
-		NBTTagCompound compound = stack.getTagCompound();
-		NBTTagCompound tiletag = new NBTTagCompound();
-		
-		TileEntity tileentity = world.getTileEntity(pos);
-		if (!(tileentity instanceof TileEntityCobbleGenerator)) {
-			return;
-		}
-		TileEntityCobbleGenerator generator = (TileEntityCobbleGenerator) tileentity;
-		generator.writeNBT(tiletag);
-		compound.setTag("TileEntityData", tiletag);
 	}
 }
