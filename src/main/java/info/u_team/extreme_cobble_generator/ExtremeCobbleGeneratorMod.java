b7a3eec77cf617a06f693bd294e95b3f5f92e252
HyCraftHD <hycrafthd@live.de>
2020-02-20 13:33:54 +0100

    Add normal main class

--- src/main/java/info/u_team/extreme_cobble_generator/ExtremeCobbleGeneratorMod.java
+++ src/main/java/info/u_team/extreme_cobble_generator/ExtremeCobbleGeneratorMod.java
@@ -1,37 +1,14 @@
 package info.u_team.extreme_cobble_generator;
 
-import static info.u_team.extreme_cobble_generator.ExtremeCobbleGeneratorConstants.*;
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
+import net.minecraftforge.fml.common.Mod;
 
-import info.u_team.extreme_cobble_generator.proxy.CommonProxy;
-import net.minecraftforge.fml.common.*;
-import net.minecraftforge.fml.common.Mod.*;
-import net.minecraftforge.fml.common.event.*;
-
-@Mod(modid = MODID, name = NAME, version = VERSION, acceptedMinecraftVersions = MCVERSION, dependencies = DEPENDENCIES, updateJSON = UPDATEURL)
+@Mod(ExtremeCobbleGeneratorMod.MODID)
 public class ExtremeCobbleGeneratorMod {
 	
-	@Instance
-	private static ExtremeCobbleGeneratorMod instance;
+	public static final String MODID = "extremecobblegenerator";
 	
-	public static ExtremeCobbleGeneratorMod getInstance() {
-		return instance;
-	}
-	
-	@SidedProxy(serverSide = COMMONPROXY, clientSide = CLIENTPROXY)
-	private static CommonProxy proxy;
-	
-	@EventHandler
-	public void preinit(FMLPreInitializationEvent event) {
-		proxy.preinit(event);
-	}
-	
-	@EventHandler
-	public void init(FMLInitializationEvent event) {
-		proxy.init(event);
-	}
-	
-	@EventHandler
-	public void postinit(FMLPostInitializationEvent event) {
-		proxy.postinit(event);
+	public ExtremeCobbleGeneratorMod() {
+		JarSignVerifier.checkSigned(MODID);
 	}
 }
