9b637c8818ba9f8b53a91c21ac534e8b37e809c0
HyCraftHD <hycrafthd@live.de>
2020-02-20 15:33:00 +0100

    Register new container type

--- src/main/java/info/u_team/extreme_cobble_generator/container/ContainerCobbleGenerator.java
+++ src/main/java/info/u_team/extreme_cobble_generator/container/ContainerCobbleGenerator.java
@@ -1,53 +1,70 @@
 package info.u_team.extreme_cobble_generator.container;
 
+import info.u_team.extreme_cobble_generator.init.ExtremeCobbleGeneratorContainerTypes;
 import info.u_team.extreme_cobble_generator.tileentity.CobbleGeneratorTileEntity;
-import info.u_team.u_team_core.container.UContainer;
-import net.minecraft.entity.player.*;
-import net.minecraft.inventory.Slot;
-import net.minecraft.item.ItemStack;
+import info.u_team.u_team_core.container.UTileEntityContainer;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.network.PacketBuffer;
 
-public class ContainerCobbleGenerator extends UContainer {
+public class ContainerCobbleGenerator extends UTileEntityContainer<CobbleGeneratorTileEntity> {
+
+	// Client
+	public ContainerCobbleGenerator(int id, PlayerInventory playerInventory, PacketBuffer buffer) {
+		super(ExtremeCobbleGeneratorContainerTypes.GENERATOR, id, playerInventory, buffer);
+	}
 	
-	public ContainerCobbleGenerator(InventoryPlayer inventory, CobbleGeneratorTileEntity generator) {
-		for (int i = 0; i < 3; ++i) {
-			for (int j = 0; j < 9; ++j) {
-				this.addSlotToContainer(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
-			}
-		}
-		
-		for (int k = 0; k < 9; ++k) {
-			this.addSlotToContainer(new Slot(inventory, k, 8 + k * 18, 142));
-		}
+	// Server
+	public ContainerCobbleGenerator(int id, PlayerInventory playerInventory, CobbleGeneratorTileEntity tileEntity) {
+		super(ExtremeCobbleGeneratorContainerTypes.GENERATOR,id, playerInventory, tileEntity);
 	}
 	
 	@Override
-	public ItemStack transferStackInSlot(EntityPlayer player, int index) {
-		ItemStack newstack = ItemStack.EMPTY;
-		Slot slot = this.inventorySlots.get(index);
-		
-		if (slot != null && slot.getHasStack()) {
-			ItemStack oldstack = slot.getStack();
-			newstack = oldstack.copy();
-			
-			if (index >= 0 && index < 27) {
-				if (!this.mergeItemStack(oldstack, 27, 36, false)) {
-					return ItemStack.EMPTY;
-				}
-			} else if (index >= 27 && index < 36 && !this.mergeItemStack(oldstack, 0, 27, false)) {
-				return ItemStack.EMPTY;
-			}
-			
-			if (oldstack.isEmpty()) {
-				slot.putStack(ItemStack.EMPTY);
-			} else {
-				slot.onSlotChanged();
-			}
-			
-			if (oldstack.getCount() == newstack.getCount()) {
-				return ItemStack.EMPTY;
-			}
-			slot.onTake(player, oldstack);
-		}
-		return newstack;
+	protected void init(boolean server) {
 	}
+
+	
+	
+	
+//	public ContainerCobbleGenerator(InventoryPlayer inventory, CobbleGeneratorTileEntity generator) {
+//		for (int i = 0; i < 3; ++i) {
+//			for (int j = 0; j < 9; ++j) {
+//				this.addSlotToContainer(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+//			}
+//		}
+//		
+//		for (int k = 0; k < 9; ++k) {
+//			this.addSlotToContainer(new Slot(inventory, k, 8 + k * 18, 142));
+//		}
+//	}
+	
+//	@Override
+//	public ItemStack transferStackInSlot(EntityPlayer player, int index) {
+//		ItemStack newstack = ItemStack.EMPTY;
+//		Slot slot = this.inventorySlots.get(index);
+//		
+//		if (slot != null && slot.getHasStack()) {
+//			ItemStack oldstack = slot.getStack();
+//			newstack = oldstack.copy();
+//			
+//			if (index >= 0 && index < 27) {
+//				if (!this.mergeItemStack(oldstack, 27, 36, false)) {
+//					return ItemStack.EMPTY;
+//				}
+//			} else if (index >= 27 && index < 36 && !this.mergeItemStack(oldstack, 0, 27, false)) {
+//				return ItemStack.EMPTY;
+//			}
+//			
+//			if (oldstack.isEmpty()) {
+//				slot.putStack(ItemStack.EMPTY);
+//			} else {
+//				slot.onSlotChanged();
+//			}
+//			
+//			if (oldstack.getCount() == newstack.getCount()) {
+//				return ItemStack.EMPTY;
+//			}
+//			slot.onTake(player, oldstack);
+//		}
+//		return newstack;
+//	}
 }
--- src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorContainerTypes.java
+++ src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorContainerTypes.java
@@ -1,6 +1,8 @@
 package info.u_team.extreme_cobble_generator.init;
 
 import info.u_team.extreme_cobble_generator.ExtremeCobbleGeneratorMod;
+import info.u_team.extreme_cobble_generator.container.ContainerCobbleGenerator;
+import info.u_team.u_team_core.containertype.UContainerType;
 import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraftforge.event.RegistryEvent.Register;
@@ -10,6 +12,8 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
 @EventBusSubscriber(modid = ExtremeCobbleGeneratorMod.MODID, bus = Bus.MOD)
 public class ExtremeCobbleGeneratorContainerTypes {
+	
+	public static final ContainerType<ContainerCobbleGenerator> GENERATOR = new UContainerType<ContainerCobbleGenerator>("generator", ContainerCobbleGenerator::new);
 	
 	@SubscribeEvent
 	public static void register(Register<ContainerType<?>> event) {
