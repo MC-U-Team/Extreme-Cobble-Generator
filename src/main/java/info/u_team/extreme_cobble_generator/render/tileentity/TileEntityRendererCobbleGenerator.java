849ec6ab297b6810759936eb8984f9177e442258
HyCraftHD <hycrafthd@live.de>
2020-02-20 15:37:17 +0100

    Comment out old render

--- src/main/java/info/u_team/extreme_cobble_generator/render/tileentity/TileEntityRendererCobbleGenerator.java
+++ src/main/java/info/u_team/extreme_cobble_generator/render/tileentity/TileEntityRendererCobbleGenerator.java
@@ -1,43 +1,33 @@
 package info.u_team.extreme_cobble_generator.render.tileentity;
 
-import info.u_team.extreme_cobble_generator.tileentity.CobbleGeneratorTileEntity;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
-import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemStack;
-
-public class TileEntityRendererCobbleGenerator extends TileEntitySpecialRenderer<CobbleGeneratorTileEntity> {
-	
-	@Override
-	public void render(CobbleGeneratorTileEntity tileentity, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
-		
-		super.render(tileentity, x, y, z, partialTicks, destroyStage, alpha);
-		
-		if (!(rendererDispatcher.entity instanceof EntityLivingBase)) {
-			return;
-		}
-		
-		if (!tileentity.isWorking()) {
-			return;
-		}
-		
-		EntityLivingBase livingbase = (EntityLivingBase) rendererDispatcher.entity;
-		
-		GlStateManager.pushMatrix();
-		GlStateManager.translate((float) x + 0.5, (float) y + 0.25, (float) z + 0.5);
-		GlStateManager.scale(1.3F, 1.3F, 1.3F);
-		GlStateManager.rotate(rendererDispatcher.entity.ticksExisted * 5, 0F, 1F, 0F);
-		GlStateManager.enableRescaleNormal();
-		bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-		
-		Minecraft.getMinecraft().getItemRenderer().renderItem(livingbase, new ItemStack(Blocks.COBBLESTONE), ItemCameraTransforms.TransformType.GROUND);
-		
-		GlStateManager.disableRescaleNormal();
-		GlStateManager.popMatrix();
-	}
-	
-}
+//public class TileEntityRendererCobbleGenerator extends TileEntitySpecialRenderer<CobbleGeneratorTileEntity> {
+//	
+//	@Override
+//	public void render(CobbleGeneratorTileEntity tileentity, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
+//		
+//		super.render(tileentity, x, y, z, partialTicks, destroyStage, alpha);
+//		
+//		if (!(rendererDispatcher.entity instanceof EntityLivingBase)) {
+//			return;
+//		}
+//		
+//		if (!tileentity.isWorking()) {
+//			return;
+//		}
+//		
+//		EntityLivingBase livingbase = (EntityLivingBase) rendererDispatcher.entity;
+//		
+//		GlStateManager.pushMatrix();
+//		GlStateManager.translate((float) x + 0.5, (float) y + 0.25, (float) z + 0.5);
+//		GlStateManager.scale(1.3F, 1.3F, 1.3F);
+//		GlStateManager.rotate(rendererDispatcher.entity.ticksExisted * 5, 0F, 1F, 0F);
+//		GlStateManager.enableRescaleNormal();
+//		bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+//		
+//		Minecraft.getMinecraft().getItemRenderer().renderItem(livingbase, new ItemStack(Blocks.COBBLESTONE), ItemCameraTransforms.TransformType.GROUND);
+//		
+//		GlStateManager.disableRescaleNormal();
+//		GlStateManager.popMatrix();
+//	}
+//	
+//}
