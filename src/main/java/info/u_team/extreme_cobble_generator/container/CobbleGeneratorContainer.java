87553b8d1a867de5a283caa9703f42e28ec38c6c
HyCraftHD <hycrafthd@live.de>
2020-02-21 05:38:34 +0100

    Add transfer slot again

--- src/main/java/info/u_team/extreme_cobble_generator/container/CobbleGeneratorContainer.java
+++ src/main/java/info/u_team/extreme_cobble_generator/container/CobbleGeneratorContainer.java
@@ -3,7 +3,9 @@ package info.u_team.extreme_cobble_generator.container;
 import info.u_team.extreme_cobble_generator.init.ExtremeCobbleGeneratorContainerTypes;
 import info.u_team.extreme_cobble_generator.tileentity.CobbleGeneratorTileEntity;
 import info.u_team.u_team_core.container.UTileEntityContainer;
-import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.*;
+import net.minecraft.inventory.container.Slot;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 
 public class CobbleGeneratorContainer extends UTileEntityContainer<CobbleGeneratorTileEntity> {
@@ -27,46 +29,34 @@ public class CobbleGeneratorContainer extends UTileEntityContainer<CobbleGenerat
 		addClientToServerTracker(tileEntity.getAmountUpdateMessage());
 	}
 	
-	// public ContainerCobbleGenerator(InventoryPlayer inventory, CobbleGeneratorTileEntity generator) {
-	// for (int i = 0; i < 3; ++i) {
-	// for (int j = 0; j < 9; ++j) {
-	// this.addSlotToContainer(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
-	// }
-	// }
-	//
-	// for (int k = 0; k < 9; ++k) {
-	// this.addSlotToContainer(new Slot(inventory, k, 8 + k * 18, 142));
-	// }
-	// }
-	
-	// @Override
-	// public ItemStack transferStackInSlot(EntityPlayer player, int index) {
-	// ItemStack newstack = ItemStack.EMPTY;
-	// Slot slot = this.inventorySlots.get(index);
-	//
-	// if (slot != null && slot.getHasStack()) {
-	// ItemStack oldstack = slot.getStack();
-	// newstack = oldstack.copy();
-	//
-	// if (index >= 0 && index < 27) {
-	// if (!this.mergeItemStack(oldstack, 27, 36, false)) {
-	// return ItemStack.EMPTY;
-	// }
-	// } else if (index >= 27 && index < 36 && !this.mergeItemStack(oldstack, 0, 27, false)) {
-	// return ItemStack.EMPTY;
-	// }
-	//
-	// if (oldstack.isEmpty()) {
-	// slot.putStack(ItemStack.EMPTY);
-	// } else {
-	// slot.onSlotChanged();
-	// }
-	//
-	// if (oldstack.getCount() == newstack.getCount()) {
-	// return ItemStack.EMPTY;
-	// }
-	// slot.onTake(player, oldstack);
-	// }
-	// return newstack;
-	// }
+	@Override
+	public ItemStack transferStackInSlot(PlayerEntity player, int index) {
+		ItemStack newstack = ItemStack.EMPTY;
+		Slot slot = this.inventorySlots.get(index);
+		
+		if (slot != null && slot.getHasStack()) {
+			ItemStack oldstack = slot.getStack();
+			newstack = oldstack.copy();
+			
+			if (index >= 0 && index < 27) {
+				if (!this.mergeItemStack(oldstack, 27, 36, false)) {
+					return ItemStack.EMPTY;
+				}
+			} else if (index >= 27 && index < 36 && !this.mergeItemStack(oldstack, 0, 27, false)) {
+				return ItemStack.EMPTY;
+			}
+			
+			if (oldstack.isEmpty()) {
+				slot.putStack(ItemStack.EMPTY);
+			} else {
+				slot.onSlotChanged();
+			}
+			
+			if (oldstack.getCount() == newstack.getCount()) {
+				return ItemStack.EMPTY;
+			}
+			slot.onTake(player, oldstack);
+		}
+		return newstack;
+	}
 }
