0e4901246e3b45198e3487342758ec79d42ce6a2
HyCraftHD <hycrafthd@live.de>
2020-02-21 05:48:28 +0100

    Generate item model

--- src/main/java/info/u_team/extreme_cobble_generator/data/ExtremeCobbleGeneratorDataGenerator.java
+++ src/main/java/info/u_team/extreme_cobble_generator/data/ExtremeCobbleGeneratorDataGenerator.java
@@ -1,7 +1,7 @@
 package info.u_team.extreme_cobble_generator.data;
 
 import info.u_team.extreme_cobble_generator.ExtremeCobbleGeneratorMod;
-import info.u_team.extreme_cobble_generator.data.provider.ExtremeCobbleGeneratorBlockStatesProvider;
+import info.u_team.extreme_cobble_generator.data.provider.*;
 import info.u_team.u_team_core.data.GenerationData;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -16,8 +16,8 @@ public class ExtremeCobbleGeneratorDataGenerator {
 		final GenerationData data = new GenerationData(ExtremeCobbleGeneratorMod.MODID, event);
 		if (event.includeClient()) {
 			data.addProvider(ExtremeCobbleGeneratorBlockStatesProvider::new);
-//			data.addProvider(ExtremeCobbleGeneratorItemModelsProvider::new);
-//			data.addProvider(ExtremeCobbleGeneratorLanguagesProvider::new);
+			data.addProvider(ExtremeCobbleGeneratorItemModelsProvider::new);
+			// data.addProvider(ExtremeCobbleGeneratorLanguagesProvider::new);
 		}
 	}
 	
---
+++ src/main/java/info/u_team/extreme_cobble_generator/data/provider/ExtremeCobbleGeneratorItemModelsProvider.java
@@ -0,0 +1,18 @@
+package info.u_team.extreme_cobble_generator.data.provider;
+
+import static info.u_team.extreme_cobble_generator.init.ExtremeCobbleGeneratorBlocks.GENERATOR;
+
+import info.u_team.u_team_core.data.*;
+
+public class ExtremeCobbleGeneratorItemModelsProvider extends CommonItemModelsProvider {
+	
+	public ExtremeCobbleGeneratorItemModelsProvider(GenerationData data) {
+		super(data);
+	}
+	
+	@Override
+	protected void registerModels() {
+		simpleBlock(GENERATOR);
+	}
+	
+}
