a07382d235de0fb227c12b5419f39a3d04630da5
HyCraftHD <hycrafthd@live.de>
2020-06-27 00:27:23 +0200

    Use the deferred register to register blocks

--- src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorBlocks.java
+++ src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorBlocks.java
@@ -1,21 +1,16 @@
 package info.u_team.extreme_cobble_generator.init;
 
-import java.util.List;
 import info.u_team.extreme_cobble_generator.ExtremeCobbleGeneratorMod;
 import info.u_team.extreme_cobble_generator.block.CobbleGeneratorBlock;
 import info.u_team.u_team_core.util.registry.*;
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.*;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraft.item.BlockItem;
+import net.minecraftforge.eventbus.api.IEventBus;
 
 public class ExtremeCobbleGeneratorBlocks {
 	
 	public static final BlockDeferredRegister BLOCKS = BlockDeferredRegister.create(ExtremeCobbleGeneratorMod.MODID);
 	
-	public static final CobbleGeneratorBlock GENERATOR = new CobbleGeneratorBlock("generator");
+	public static final BlockRegistryObject<CobbleGeneratorBlock, BlockItem> GENERATOR = BLOCKS.register("generator", CobbleGeneratorBlock::new);
 	
 	public static void register(IEventBus bus) {
 		BLOCKS.register(bus);
