29da0e47c287a5d0d5a840a0c59f7f0c7cf6fe6c
HyCraftHD <hycrafthd@live.de>
2020-06-27 00:28:49 +0200

    Use the deferred register to register the generator

--- src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorContainerTypes.java
+++ src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorContainerTypes.java
@@ -6,13 +6,14 @@ import info.u_team.u_team_core.containertype.UContainerType;
 import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ExtremeCobbleGeneratorContainerTypes {
 	
 	public static final CommonDeferredRegister<ContainerType<?>> CONTAINER_TYPES = CommonDeferredRegister.create(ForgeRegistries.CONTAINERS, ExtremeCobbleGeneratorMod.MODID);
 	
-	public static final ContainerType<CobbleGeneratorContainer> GENERATOR = new UContainerType<CobbleGeneratorContainer>("generator", CobbleGeneratorContainer::new);
+	public static final RegistryObject<ContainerType<CobbleGeneratorContainer>> GENERATOR = CONTAINER_TYPES.register("generator", () -> new UContainerType<CobbleGeneratorContainer>(CobbleGeneratorContainer::new));
 	
 	public static void register(IEventBus bus) {
 		CONTAINER_TYPES.register(bus);
