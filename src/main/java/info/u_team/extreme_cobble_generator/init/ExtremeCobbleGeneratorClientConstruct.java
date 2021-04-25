a2a313c53838e0392f974011c805156bddf82ea9
HyCraftHD <hycrafthd@live.de>
2021-04-25 15:05:45 +0200

    Update render type registry

--- src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorClientConstruct.java
+++ src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorClientConstruct.java
@@ -3,12 +3,14 @@ package info.u_team.extreme_cobble_generator.init;
 import info.u_team.extreme_cobble_generator.ExtremeCobbleGeneratorMod;
 import info.u_team.u_team_core.api.construct.Construct;
 import info.u_team.u_team_core.api.construct.IModConstruct;
+import info.u_team.u_team_core.util.registry.BusRegister;
 
 @Construct(modid = ExtremeCobbleGeneratorMod.MODID, client = true)
 public class ExtremeCobbleGeneratorClientConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
+		BusRegister.registerMod(ExtremeCobbleGeneratorRenderTypes::registerMod);
 	}
 	
 }
--- src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorRenderTypes.java
+++ src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorRenderTypes.java
@@ -2,24 +2,18 @@ package info.u_team.extreme_cobble_generator.init;
 
 import static info.u_team.extreme_cobble_generator.init.ExtremeCobbleGeneratorBlocks.GENERATOR;
 
-import info.u_team.extreme_cobble_generator.ExtremeCobbleGeneratorMod;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = ExtremeCobbleGeneratorMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
 public class ExtremeCobbleGeneratorRenderTypes {
 	
-	@SubscribeEvent
-	public static void register(FMLClientSetupEvent event) {
-		// Cutout
-		final RenderType cutout = RenderType.getCutout();
-		
-		RenderTypeLookup.setRenderLayer(GENERATOR.get(), cutout);
+	private static void setup(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(GENERATOR.get(), RenderType.getCutout());
 	}
 	
+	public static void registerMod(IEventBus bus) {
+		bus.addListener(ExtremeCobbleGeneratorRenderTypes::setup);
+	}
 }
