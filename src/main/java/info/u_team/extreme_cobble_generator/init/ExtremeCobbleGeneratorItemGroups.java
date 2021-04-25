e524f58d5253f50f8276acb83ebb7bf6f22b3896
HyCraftHD <hycrafthd@live.de>
2021-04-25 15:04:19 +0200

    Use specific type in constant

--- src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorItemGroups.java
+++ src/main/java/info/u_team/extreme_cobble_generator/init/ExtremeCobbleGeneratorItemGroups.java
@@ -2,10 +2,9 @@ package info.u_team.extreme_cobble_generator.init;
 
 import info.u_team.extreme_cobble_generator.ExtremeCobbleGeneratorMod;
 import info.u_team.u_team_core.itemgroup.UItemGroup;
-import net.minecraft.item.ItemGroup;
 
 public class ExtremeCobbleGeneratorItemGroups {
 	
-	public static final ItemGroup GROUP = new UItemGroup(ExtremeCobbleGeneratorMod.MODID, "group", ExtremeCobbleGeneratorBlocks.GENERATOR);
+	public static final UItemGroup GROUP = new UItemGroup(ExtremeCobbleGeneratorMod.MODID, "group", ExtremeCobbleGeneratorBlocks.GENERATOR);
 	
 }
