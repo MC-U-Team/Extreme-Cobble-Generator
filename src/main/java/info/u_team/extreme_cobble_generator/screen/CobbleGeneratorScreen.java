21f95aa1bfd3963f346ae1e6a4c05db76a882168
HyCraftHD <hycrafthd@live.de>
2021-04-25 15:33:08 +0200

    Update screen, not compile errors anymore

--- src/main/java/info/u_team/extreme_cobble_generator/screen/CobbleGeneratorScreen.java
+++ src/main/java/info/u_team/extreme_cobble_generator/screen/CobbleGeneratorScreen.java
@@ -1,30 +1,34 @@
 package info.u_team.extreme_cobble_generator.screen;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+
 import info.u_team.extreme_cobble_generator.ExtremeCobbleGeneratorMod;
 import info.u_team.extreme_cobble_generator.container.CobbleGeneratorContainer;
 import info.u_team.extreme_cobble_generator.tileentity.CobbleGeneratorTileEntity;
 import info.u_team.u_team_core.gui.elements.EnergyStorageWidget;
-import info.u_team.u_team_core.screen.UContainerScreen;
+import info.u_team.u_team_core.gui.elements.ScalableButton;
+import info.u_team.u_team_core.gui.elements.ScalableSlider;
+import info.u_team.u_team_core.screen.UBasicContainerScreen;
 import io.netty.buffer.Unpooled;
-import net.minecraft.client.resources.I18n;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class CobbleGeneratorScreen extends UContainerScreen<CobbleGeneratorContainer> {
+public class CobbleGeneratorScreen extends UBasicContainerScreen<CobbleGeneratorContainer> {
 	
 	private static final ResourceLocation BACKGROUND = new ResourceLocation(ExtremeCobbleGeneratorMod.MODID, "textures/gui/cobblegenerator.png");
 	
-	private BetterFontSlider slider;
+	private ScalableSlider slider;
 	
 	public CobbleGeneratorScreen(CobbleGeneratorContainer container, PlayerInventory playerInventory, ITextComponent title) {
-		super(container, playerInventory, title, BACKGROUND);
-		xSize = 176;
-		ySize = 173;
+		super(container, playerInventory, title, BACKGROUND, 176, 173);
 	}
 	
 	@Override
@@ -35,14 +39,14 @@ public class CobbleGeneratorScreen extends UContainerScreen<CobbleGeneratorConta
 		
 		addButton(new EnergyStorageWidget(guiLeft + 9, guiTop + 20, 54, () -> tileEntity.getInternalEnergyStorage()));
 		
-		addButton(new BetterButton(guiLeft + 36, guiTop + 20, 15, 15, 0.75F, "+", button -> sendUpdateAddValueMessage(100)));
-		addButton(new BetterButton(guiLeft + 56, guiTop + 20, 15, 15, 0.75F, "+", button -> sendUpdateAddValueMessage(10)));
-		addButton(new BetterButton(guiLeft + 76, guiTop + 20, 15, 15, 0.75F, "+", button -> sendUpdateAddValueMessage(1)));
-		addButton(new BetterButton(guiLeft + 101, guiTop + 20, 15, 15, 0.75F, "-", button -> sendUpdateAddValueMessage(-1)));
-		addButton(new BetterButton(guiLeft + 121, guiTop + 20, 15, 15, 0.75F, "-", button -> sendUpdateAddValueMessage(-10)));
-		addButton(new BetterButton(guiLeft + 141, guiTop + 20, 15, 15, 0.75F, "-", button -> sendUpdateAddValueMessage(-100)));
+		addButton(new ScalableButton(guiLeft + 36, guiTop + 20, 15, 15, new StringTextComponent("+"), 0.75F, button -> sendUpdateAddValueMessage(100)));
+		addButton(new ScalableButton(guiLeft + 56, guiTop + 20, 15, 15, new StringTextComponent("+"), 0.75F, button -> sendUpdateAddValueMessage(10)));
+		addButton(new ScalableButton(guiLeft + 76, guiTop + 20, 15, 15, new StringTextComponent("+"), 0.75F, button -> sendUpdateAddValueMessage(1)));
+		addButton(new ScalableButton(guiLeft + 101, guiTop + 20, 15, 15, new StringTextComponent("-"), 0.75F, button -> sendUpdateAddValueMessage(-1)));
+		addButton(new ScalableButton(guiLeft + 121, guiTop + 20, 15, 15, new StringTextComponent("-"), 0.75F, button -> sendUpdateAddValueMessage(-10)));
+		addButton(new ScalableButton(guiLeft + 141, guiTop + 20, 15, 15, new StringTextComponent("-"), 0.75F, button -> sendUpdateAddValueMessage(-100)));
 		
-		slider = addButton(new BetterFontSlider(guiLeft + 36, guiTop + 40, 120, 15, I18n.format("container.extremecobblegenerator.generator.amount") + ": ", "", 0, tileEntity.maxGeneration, tileEntity.getAmount(), false, true, 0.75F, null) {
+		slider = addButton(new ScalableSlider(guiLeft + 36, guiTop + 40, 120, 15, new TranslationTextComponent("container.extremecobblegenerator.generator.amount").appendString(": "), new StringTextComponent(""), 0, tileEntity.maxGeneration, tileEntity.getAmount(), false, true, true, 0.75F) {
 			
 			@Override
 			public void onRelease(double mouseX, double mouseY) {
@@ -62,28 +66,22 @@ public class CobbleGeneratorScreen extends UContainerScreen<CobbleGeneratorConta
 	}
 	
 	@Override
-	public void render(int mouseX, int mouseY, float partialTicks) {
-		renderBackground();
-		super.render(mouseX, mouseY, partialTicks);
-		buttons.forEach(button -> button.renderToolTip(mouseX, mouseY));
-		renderHoveredToolTip(mouseX, mouseY);
-		
+	public void drawGuiContainerForegroundLayer(MatrixStack matrixStack, int mouseX, int mouseY) {
+		font.func_238418_a_(new TranslationTextComponent("container.extremecobblegenerator.generator.description", container.getTileEntity().getAmount() * 20), 36, 58, 120, 0x404040);
+	}
+	
+	@Override
+	protected void drawGuiContainerBackgroundLayer(MatrixStack matrixStack, float partialTicks, int mouseX, int mouseY) {
+		super.drawGuiContainerBackgroundLayer(matrixStack, partialTicks, mouseX, mouseY);
+		blit(matrixStack, guiLeft + 155, guiTop + 75, 10, 10, xSize + (container.getTileEntity().isWorking() ? 32 : 0), 0, 32, 32, backgroundWidth, backgroundHeight);
+	}
+	
+	@Override
+	public void renderToolTip(MatrixStack matrixStack, Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
+		super.renderToolTip(matrixStack, minecraft, mouseX, mouseY, partialTicks);
 		if (isPointInRegion(154, 74, 12, 12, mouseX, mouseY)) {
-			renderTooltip(container.getTileEntity().isWorking() ? I18n.format("container.extremecobblegenerator.generator.working") : I18n.format("container.extremecobblegenerator.generator.idling"), mouseX, mouseY);
+			renderTooltip(matrixStack, container.getTileEntity().isWorking() ? new TranslationTextComponent("container.extremecobblegenerator.generator.working") : new TranslationTextComponent("container.extremecobblegenerator.generator.idling"), mouseX, mouseY);
 		}
-	}
-	
-	@Override
-	public void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-		font.drawString(title.getFormattedText(), 8, 6, 4210752);
-		font.drawString(playerInventory.getDisplayName().getFormattedText(), 8.0F, ySize - 94, 4210752);
-		font.drawSplitString(I18n.format("container.extremecobblegenerator.generator.description", container.getTileEntity().getAmount() * 20), 36, 58, 120, 4210752);
-	}
-	
-	@Override
-	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-		super.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
-		blit(guiLeft + 155, guiTop + 75, 10, 10, xSize + (container.getTileEntity().isWorking() ? 32 : 0), 0, 32, 32, backgroundWidth, backgroundHeight);
 	}
 	
 	@Override
