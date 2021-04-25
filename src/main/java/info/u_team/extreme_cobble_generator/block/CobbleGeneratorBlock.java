f092e02c42ad99efed666d780c2d434725100197
HyCraftHD <hycrafthd@live.de>
2021-04-25 15:34:03 +0200

    Supress depcreation warning

--- src/main/java/info/u_team/extreme_cobble_generator/block/CobbleGeneratorBlock.java
+++ src/main/java/info/u_team/extreme_cobble_generator/block/CobbleGeneratorBlock.java
@@ -59,6 +59,7 @@ public class CobbleGeneratorBlock extends UTileEntityBlock {
 		return state.with(FACING, rotation.rotate(state.get(FACING)));
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Override
 	public BlockState mirror(BlockState state, Mirror mirror) {
 		return state.rotate(mirror.toRotation(state.get(FACING)));
